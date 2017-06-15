ee16bcd4245f521c938990af29be1e4915bdbe5a
Challen <273239067@qq.com>
2017-06-15 13:55:49 +0800

    New ROI trace back based on M2 segmention

--- ROI_volmue_283_MNI.m
+++ ROI_volmue_283_MNI.m
@@ -1,16 +1,17 @@
 %function:统计一个目录下所有分割结果中283个脑区的ROI大小，放到一个txt文档中
 clc;clear all;close all;
-pathname_G1 = 'I:\NC30';
+pathname_G1 = 'G:\m2question';
 cd(pathname_G1);
 dirs_G1=dir([pathname_G1, '\*.*']);
 dircell_G1=struct2cell(dirs_G1)' ;
-filenames_G1=dircell_G1(3:length(dircell_G1),1);
+filenames_G1=dircell_G1(3:length(dircell_G1)-1,1);
 %file_path_G1=fullfile(pathname_G1,filenames_G1); %保存各网络参数的全路径
 dirs_G2=dir([pathname_G1, '\*.*']);
 dircell_G2=struct2cell(dirs_G2)' ;
-filenames_G2=dircell_G2(3:length(dircell_G2),1);
+filenames_G2=dircell_G2(3:length(dircell_G2)-1,1);
 savemat = zeros(length(filenames_G1),283);
  for i=1:length(filenames_G1)
+    cd(char(filenames_G1(i)));
     cd(char(filenames_G1(i)));
     cd output;
     clear dirs_G2;clear dircell_G2;clear filenames_G2;
@@ -21,7 +22,7 @@ savemat = zeros(length(filenames_G1),283);
     for j = 1:len
        
         filenamesIS = char(filenames_G2(j));
-        jamar = strfind(filenamesIS,'283Labels_M2_MNI.img');
+        jamar = strfind(filenamesIS,'283Labels_MNI.img');
         if length(jamar) ~= 0 
            
             [hdr,filetype,fileprefix,machine] = load_nii_hdr(filenamesIS);
